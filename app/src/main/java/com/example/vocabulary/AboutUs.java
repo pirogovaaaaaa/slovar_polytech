e3bd78d0158136985c82837e1ac75a8dcceb7165
Николай <asakura475@gmail.com>
2019-04-17 09:36:19 +0300

    Android app test commit

---
+++ app/src/main/java/com/example/vocabulary/AboutUs.java
@@ -0,0 +1,15 @@
+package com.example.vocabulary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_us);
+
+        // TODO Настрой это окно!
+    }
+}
