627f43b40047008f8b533718909deb80d106a4f9
Hortule <hortule@gmail.com>
2019-04-21 20:39:23 +0300

    database and api done (#2)
    * database and api
    
    database and api done
    
    * fix
    
    fix extencion

---
+++ api/main.py
@@ -0,0 +1,34 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+import postgresql
+
+
+app = Flask(__name__)
+
+
+def db_conn():
+    """connect to db"""
+    return postgresql.open('pq://postgres:password@localhost:5432/date')
+
+
+@app.route('/api/v1.0/terms/<string:naprav>', methods=['GET'])
+def get_terms(naprav):
+    """return term, opred_id and naprav from db"""
+    with db_conn() as db:
+        terms = db.query('SELECT term, opred_id, naprav FROM terms\n'
+                         '        WHERE naprav = $1', naprav)
+        return jsonify({'terms': terms}), 201
+
+
+@app.route('/api/v1.0/opreds/<int:opred_id>', methods=['GET'])
+def get_opreds(opred_id):
+    """return opreds from db"""
+    with db_conn() as db:
+        opreds = db.query('SELECT opred_id, opred FROM opreds\n'
+                          '        WHERE opred_id = $1', opred_id)
+        return jsonify({'opreds': opreds}), 201
+
+
+if __name__ == '__main__':
+    """disable debug mode and enable access from internet"""
+    app.run(host='0.0.0.0', debug=False)
