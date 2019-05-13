b151e9162afb96150c57a79ced610e1c4e5e2cb5
Hortule <hortule@gmail.com>
2019-05-13 19:00:57 +0300

    new and fix
    new api methods and fixed response codes

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 #!flask/bin/python
 from flask import Flask, jsonify
 import postgresql
+from urllib.parse import unquote
 
 
 app = Flask(__name__)
@@ -17,7 +18,7 @@ def get_terms(naprav):
     with db_conn() as db:
         terms = db.query('SELECT term, opred_id, naprav FROM terms\n'
                          '        WHERE naprav = $1', naprav)
-        return jsonify({'terms': terms}), 201
+        return jsonify({'terms': terms}), 200
 
 
 @app.route('/api/v1.0/opreds/<int:opred_id>', methods=['GET'])
@@ -26,7 +27,25 @@ def get_opreds(opred_id):
     with db_conn() as db:
         opreds = db.query('SELECT opred_id, opred FROM opreds\n'
                           '        WHERE opred_id = $1', opred_id)
-        return jsonify({'opreds': opreds}), 201
+        return jsonify({'opreds': opreds}), 200
+
+
+@app.route('/api/v1.0/napravs/', methods=['GET'])
+def get_fktheplc():
+    """return naprav from db"""
+    with db_conn() as db:
+        napravs = db.query('SELECT DISTINCT naprav FROM terms')
+        return jsonify({'napravs': napravs}), 200
+
+
+@app.route('/api/v1.0/term/<string:term>', methods=['GET'])
+def get_acab(term):
+    """return term, opred_id and naprav from db"""
+    term = unquote(term)
+    with db_conn() as db:
+        term = db.query('SELECT term, opred_id, naprav FROM terms\n'
+                         '        WHERE lower(term) = lower($1)', term)
+        return jsonify({'term': term}), 200
 
 
 if __name__ == '__main__':
