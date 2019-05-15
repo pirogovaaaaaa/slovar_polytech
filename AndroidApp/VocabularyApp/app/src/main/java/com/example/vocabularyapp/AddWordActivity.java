114f356eba0cf3c20317ce41924be38a3458db55
dickoo <asakura475@gmail.com>
2019-05-15 11:04:46 +0300

    version 1.2 is ready! add word finish(?)

--- AndroidApp/VocabularyApp/app/src/main/java/com/example/vocabularyapp/AddWordActivity.java
+++ AndroidApp/VocabularyApp/app/src/main/java/com/example/vocabularyapp/AddWordActivity.java
@@ -2,14 +2,31 @@ package com.example.vocabularyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddWordActivity extends AppCompatActivity {
+
+    TextView textAddWord, textDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_word);
 
-        // TODO Применить настрой
+        textAddWord = findViewById(R.id.textAddWord);
+        textDescription = findViewById(R.id.textDescription);
+    }
+
+    public void okButton(View v) {
+        textAddWord.setText("");
+        textDescription.setText("");
+
+        Toast toast = Toast.makeText(
+                getApplicationContext(), "Слово отправлено на модерацию!",
+                Toast.LENGTH_SHORT
+        );
+        toast.show();
     }
 }
