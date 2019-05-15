7a9395a91a547b48f499c1aaa95f9e9d0e0b8115
dickoo <asakura475@gmail.com>
2019-05-15 10:55:31 +0300

    version 1.1 is ready!

--- AndroidApp/VocabularyApp/app/src/main/java/com/example/vocabularyapp/NapravListActivity.java
+++ AndroidApp/VocabularyApp/app/src/main/java/com/example/vocabularyapp/NapravListActivity.java
@@ -16,7 +16,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class NapravListActivity extends AppCompatActivity implements FindWordInterface, FindOprInterface {
+public class NapravListActivity extends AppCompatActivity implements FindOprInterface {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,44 +31,45 @@ public class NapravListActivity extends AppCompatActivity implements FindWordInt
 
         try {
             JSONObject jsonObject = new JSONObject(s);
-            JSONArray array = jsonObject.getJSONArray(jsonObject.keys().next());
+            JSONArray jsonArray = jsonObject.getJSONArray(jsonObject.keys().next());
 
-            String[]  textArray = new String[array.length()];
-            for (int i = 0; i < array.length(); ++i) {
-                String temporaryStr = array.getString(i);
+            String[]  textArray = new String[jsonArray.length()];
+            final String[] wordsId = new String[jsonArray.length()];
+            for (int i = 0; i < jsonArray.length(); ++i) {
+                String temporaryStr = jsonArray.getString(i);
                 temporaryStr = temporaryStr.substring(1, temporaryStr.length() - 1);
                 String[] temporaryArray = temporaryStr.split(",");
                 textArray[i] = temporaryArray[0].substring(1, temporaryArray[0].length() - 1);
+                wordsId[i] = temporaryArray[1];
             }
 
             ArrayAdapter<String> adapter = new ArrayAdapter<>(
                     this, android.R.layout.simple_list_item_1, textArray
             );
+
+            listNaprav.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    if (!IsOnline.isOnline()) {
+                        Toast toast = Toast.makeText(
+                                getApplicationContext(), "Нет подключения к интернету!",
+                                Toast.LENGTH_SHORT
+                        );
+                        toast.show();
+
+                        return;
+                    }
+                    FindOprOfWord findOprOfWord = new FindOprOfWord();
+                    findOprOfWord.delegate = NapravListActivity.this;
+                    findOprOfWord.execute(
+                            "http://116.203.41.4:5000/api/v1.0/opreds/" + wordsId[position]);
+
+                }
+            });
             listNaprav.setAdapter(adapter);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
-        listNaprav.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (!IsOnline.isOnline()) {
-                    Toast toast = Toast.makeText(
-                            getApplicationContext(), "Нет подключения к интернету!",
-                            Toast.LENGTH_SHORT
-                    );
-                    toast.show();
-
-                    return;
-                }
-                FindWordId findWordId = new FindWordId();
-                findWordId.delegate = NapravListActivity.this;
-                findWordId.execute(
-                        "http://116.203.41.4:5000/api/v1.0/term/" + ((TextView) view).getText()
-                );
-
-            }
-        });
     }
 
     @Override
@@ -96,25 +97,6 @@ public class NapravListActivity extends AppCompatActivity implements FindWordInt
 
             AlertDialog alertDialog = builder.create();
             alertDialog.show();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void wordFind(String output) {
-        try {
-            JSONObject jsonObject = new JSONObject(output);
-            JSONArray jsonArray = jsonObject.getJSONArray(jsonObject.keys().next());
-
-            String temporaryStr = jsonArray.getString(0);
-            temporaryStr = temporaryStr.substring(1, temporaryStr.length() - 1);
-            String[] temporaryArray = temporaryStr.split("[,\"]");
-
-            FindOprOfWord findOprOfWord = new FindOprOfWord();
-            findOprOfWord.delegate = this;
-            findOprOfWord.execute(
-                    "http://116.203.41.4:5000/api/v1.0/opreds/" + temporaryArray[3]);
         } catch (JSONException e) {
             e.printStackTrace();
         }
